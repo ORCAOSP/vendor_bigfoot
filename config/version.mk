acb053fc5c6a45ae4c9256dbe01ee9ed2c97e856
orcaosp <orcaproject@yahoo.com>
2013-04-15 22:34:31 -0400

    new stuff

--- config/version.mk
+++ config/version.mk
@@ -9,28 +9,36 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.goo.developerid=drewgaren \
     ro.goo.rom=Bigfoot \
     ro.goo.version=$(DATE) \
-    ro.orca.version=BIGFOOT-STABLE-$(TARGET_PRODUCT)-$(BIGFOOT_BUILD)
+    ro.bigfoot.version=BIGFOOT-4.2-v"$(BIGFOOT_VERSION_MAJOR).$(BIGFOOT_VERSION_MINOR).$(BIGFOOT_VERSION_MAINTAINENCE)
+    
 else
 # Bigfoot Nightlies properties
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.goo.developerid=drewgaren \
     ro.goo.rom=Bigfoot_nightly \
     ro.goo.version=$(DATE) \
-    ro.orca.version=BIGFOOT-NIGHTLY-$(TARGET_PRODUCT)-OTA-$(DATE)
+    ro.bigfoot.version=BIGFOOT-NIGHTLY-"$(BIGFOOT_VERSION_MAJOR).$(BIGFOOT_VERSION_MINOR).$(BIGFOOT_VERSION_MAINTAINENCE)-$(DATE)
 endif
 
 ## BIGFOOT Version prop ##
+
+# New Stuff
 DATE = $(shell date -u +%Y%m%d)
+BIGFOOT_VERSION_MAJOR = 1
+BIGFOOT_VERSION_MINOR = 1
+BIGFOOT_VERSION_MAINTAINENCE = 2
+
+BIGFOOT_VERSION := "BIGFOOT-4.2-v"$(BIGFOOT_VERSION_MAJOR).$(BIGFOOT_VERSION_MINOR).$(BIGFOOT_VERSION_MAINTAINENCE)
 
 # For System Information & Build zip name
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.bigfoot.version=$(TARGET_PRODUCT)-OFFICIAL-1.1.1 \
-    ro.modversion=$(TARGET_PRODUCT)_1.1.1_$(DATE)
+    ro.bigfoot.version=$(BIGFOOT_VERSION) \
+    ro.modversion=$(TARGET_PRODUCT)_$(BIGFOOT_VERSION_MAJOR).$(BIGFOOT_VERSION_MINOR).$(BIGFOOT_VERSION_MAINTAINENCE)_$(DATE)
 
 # For BFSTATS
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.bigfoot.isitrom=bigfoot \
     ro.bigfoot.modname=ORCA-BIGFOOT \
-    ro.bigfoot.modversion=1.1.1-$(DATE) \
+    ro.bigfoot.modversion=1.1.2-$(DATE) \
     ro.bigfoot.device=$(TARGET_PRODUCT) \
     ro.bigfoot.builtdate=$(DATE)
