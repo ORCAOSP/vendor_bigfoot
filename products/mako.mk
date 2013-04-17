75554883390a1c35973ee3d870f6a87a17953beb
Orca Project <orcaproject@yahoo.com>
2013-04-17 03:21:56 -0300

    build fix

--- products/mako.mk
+++ products/mako.mk
@@ -25,6 +25,6 @@ PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=occam BUILD_FINGERPRINT=google/occa
 
 # Copy Mako specific prebuilts
 PRODUCT_COPY_FILES += \
-    vendor/bigfoot/prebuilt/xhdpi/bootanimation.zip:system/media/bootanimation.zip
+    vendor/bigfoot/prebuilt/xhdpi/bootanimation.zip:system/media/bootanimation.zip \
     vendor/bigfoot/prebuilt/common/media/LMprec_508.emd:system/media/LMprec_508.emd \
     vendor/bigfoot/prebuilt/common/media/PFFprec_600.emd:system/media/PFFprec_600.emd
