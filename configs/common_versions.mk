d8c804c6d21c764b3b5ef97b80540b77689a0dd1
Orca Project <orcaproject@yahoo.com>
2013-04-18 18:20:00 -0300

    remove goo board

--- configs/common_versions.mk
+++ configs/common_versions.mk
@@ -9,7 +9,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.goo.developerid=drewgaren \
     ro.goo.rom=Bigfoot \
     ro.goo.version=$(DATE) \
-    ro.goo.board=$(TARGET_PRODUCT)
     ro.bigfoot.version=BIGFOOT-1.1.3-$(TARGET_PRODUCT)-$(DATE)
 else
 # Bigfoot Nightlies properties
@@ -17,7 +16,6 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.goo.developerid=drewgaren \
     ro.goo.rom=Bigfoot_Nightlies \
     ro.goo.version=$(DATE) \
-    ro.goo.board=$(TARGET_PRODUCT)
     ro.bigfoot.version=BIGFOOT-1.1.3-$(TARGET_PRODUCT)-Nightly-$(DATE)
 endif
 
