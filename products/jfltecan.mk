4cb1356b73164f9eca47ea5bd9ce1c3cef29c498
orcaosp <orcaproject@yahoo.com>
2013-05-09 20:50:20 -0400

    JFLTE Device

---
+++ products/jfltecan.mk
@@ -0,0 +1,39 @@
+# Inherit AOSP device configuration for t0lte.
+$(call inherit-product, device/samsung/jflteatt/full_jfltecan.mk)
+
+# Inherit GSM common stuff
+$(call inherit-product, vendor/bigfoot/configs/gsm.mk)
+
+# Inherit Bigfoot common bits
+$(call inherit-product, vendor/bigfoot/configs/common.mk)
+
+# Galaxy S4 Overlays
+PRODUCT_PACKAGE_OVERLAYS += vendor/bigfoot/overlay/s4-common
+
+# PA OVERLAY_TARGET
+OVERLAY_TARGET := pa_xhdpi
+
+# Setup device specific product configuration.
+PRODUCT_NAME := bigfoot_jfltecan
+PRODUCT_BRAND := Samsung
+PRODUCT_DEVICE := jfltecan
+PRODUCT_MODEL := SGH-I337M
+PRODUCT_MANUFACTURER := samsung
+
+# Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltevl TARGET_DEVICE=jfltecan BUILD_FINGERPRINT="samsung/jfltevl/jfltecan:4.2.2/JDQ39/I337MVLUAMDJ:user/release-keys" PRIVATE_BUILD_DESC="jfltevl-user 4.2.2 JDQ39 I337MVLUAMDJ release-keys"
+PRODUCT_RELEASE_NAME := jfltecan
+
+# Copy mako specific prebuilts
+PRODUCT_COPY_FILES += \
+    vendor/bigfoot/prebuilt/xhdpi/bootanimation.zip:system/media/bootanimation.zip \
+    vendor/bigfoot/prebuilt/preferences/images/phablet.png:system/etc/paranoid/preferences/images/phablet.png \
+    vendor/bigfoot/prebuilt/preferences/images/phone.png:system/etc/paranoid/preferences/images/phone.png \
+    vendor/bigfoot/prebuilt/preferences/images/tablet.png:system/etc/paranoid/preferences/images/tablet.png \
+    vendor/bigfoot/prebuilt/preferences/images/undefined.png:system/etc/paranoid/preferences/images/undefined.png \
+    vendor/bigfoot/prebuilt/preferences/pa_xhdpi/0_colors.xml:system/etc/paranoid/preferences/0_colors.xml \
+    vendor/bigfoot/prebuilt/preferences/pa_xhdpi/pref_1.xml:system/etc/paranoid/preferences/pref_1.xml \
+    vendor/bigfoot/prebuilt/preferences/pa_xhdpi/pref_2.xml:system/etc/paranoid/preferences/pref_2.xml \
+    vendor/bigfoot/prebuilt/preferences/pa_xhdpi/pref_3.xml:system/etc/paranoid/preferences/pref_3.xml \
+    vendor/bigfoot/prebuilt/preferences/pa_xhdpi/pref_4.xml:system/etc/paranoid/preferences/pref_4.xml \
+    vendor/bigfoot/prebuilt/preferences/pa_xhdpi/pref_5.xml:system/etc/paranoid/preferences/pref_5.xml
