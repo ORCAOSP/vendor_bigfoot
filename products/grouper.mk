fbae08a15d9cfa6e28f092661cd0f2c10ba8d528
Orca Project <shamukillaz@orcaproject.co>
2013-06-13 03:08:27 -0400

    fix grouper and tilapia build

--- products/grouper.mk
+++ products/grouper.mk
@@ -20,16 +20,16 @@ PRODUCT_MANUFACTURER := asus
 # Set build fingerprint / ID / Product Name ect.
 PRODUCT_BUILD_PROP_OVERRIDES := PRODUCT_NAME=nakasi BUILD_FINGERPRINT=google/nakasi/grouper:4.1.1/JRO03H/405518:user/release-keys PRIVATE_BUILD_DESC="nakasi-user 4.1.1 JRO03H 405518 release-keys" BUILD_NUMBER=405518
 
-# Copy mako specific prebuilts
+# Copy Mako specific prebuilts
 PRODUCT_COPY_FILES += \
     vendor/bigfoot/prebuilt/xhdpi/bootanimation.zip:system/media/bootanimation.zip \
     vendor/bigfoot/prebuilt/preferences/images/phablet.png:system/etc/paranoid/preferences/images/phablet.png \
     vendor/bigfoot/prebuilt/preferences/images/phone.png:system/etc/paranoid/preferences/images/phone.png \
     vendor/bigfoot/prebuilt/preferences/images/tablet.png:system/etc/paranoid/preferences/images/tablet.png \
     vendor/bigfoot/prebuilt/preferences/images/undefined.png:system/etc/paranoid/preferences/images/undefined.png \
-    vendor/bigfoot/prebuilt/preferences/pa_tvdpi/0_colors.xml:system/etc/paranoid/preferences/0_colors.xml \
-    vendor/bigfoot/prebuilt/preferences/pa_tvdpi/pref_1.xml:system/etc/paranoid/preferences/pref_1.xml \
-    vendor/bigfoot/prebuilt/preferences/pa_tvdpi/pref_2.xml:system/etc/paranoid/preferences/pref_2.xml \
-    vendor/bigfoot/prebuilt/preferences/pa_tvdpi/pref_3.xml:system/etc/paranoid/preferences/pref_3.xml \
-    vendor/bigfoot/prebuilt/preferences/pa_tvdpi/pref_4.xml:system/etc/paranoid/preferences/pref_4.xml \
-    vendor/bigfoot/prebuilt/preferences/pa_tvdpi/pref_5.xml:system/etc/paranoid/preferences/pref_5.xml
+    vendor/bigfoot/prebuilt/preferences/pa_xhdpi/0_colors.xml:system/etc/paranoid/preferences/0_colors.xml \
+    vendor/bigfoot/prebuilt/preferences/pa_xhdpi/pref_1.xml:system/etc/paranoid/preferences/pref_1.xml \
+    vendor/bigfoot/prebuilt/preferences/pa_xhdpi/pref_2.xml:system/etc/paranoid/preferences/pref_2.xml \
+    vendor/bigfoot/prebuilt/preferences/pa_xhdpi/pref_3.xml:system/etc/paranoid/preferences/pref_3.xml \
+    vendor/bigfoot/prebuilt/preferences/pa_xhdpi/pref_4.xml:system/etc/paranoid/preferences/pref_4.xml \
+    vendor/bigfoot/prebuilt/preferences/pa_xhdpi/pref_5.xml:system/etc/paranoid/preferences/pref_5.xml
